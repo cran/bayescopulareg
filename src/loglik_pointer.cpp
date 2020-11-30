b5c832af983a22988ba730bc10f390a53a4a7e79
Ethan Alt <ethanalt@live.unc.edu>
2020-11-30 16:10:06 +0000

    version 0.1.3

--- src/loglik_pointer.cpp
+++ src/loglik_pointer.cpp
@@ -77,7 +77,7 @@ double loglik_gamma(
   b = phi * mu;
   
   loglik_i = (a - 1) * log(y) - y / b - a * log(b);
-  return sum(loglik_i) - n * lgamma(a);
+  return arma::accu(loglik_i) - n * lgamma(a);
 }
 
 
@@ -110,8 +110,8 @@ double loglik_binomial(
   eta = X * beta;
   p = linkinv_cpp( eta, linkname );
   
-  loglik_i = y * log(p) + ( 1 - y ) * log(1 - p);
-  return sum(loglik_i);
+  loglik_i = y % log(p) + ( 1 - y ) % log(1 - p);
+  return arma::accu(loglik_i);
 }
 
 
@@ -145,8 +145,8 @@ double loglik_poisson(
   eta      = X * beta;
   lambda   = linkinv_cpp( eta, linkname );
   
-  loglik_i = y * log(lambda) - lambda;
-  return sum(loglik_i);
+  loglik_i = y % log(lambda) - lambda;
+  return arma::accu(loglik_i);
 }
 
 
@@ -206,5 +206,94 @@ double loglik_cpp(
   double l = fun( y, X, beta, phi, linkname, n );            // evaluate the log likelihood
   return l;
 }
+
+
+
+
+// // [[Rcpp::export]]
+// arma::vec invlink_cpp(
+//   arma::vec eta,
+//   const std::string& linkname
+// ) {
+//   if ( linkname == "identity" ) {
+//     return eta;
+//   }
+//   else if ( linkname == "log" ) {
+//     return arma::exp(eta);
+//   }
+//   else if ( linkname == "logit" ) {
+//     return arma::pow( 1 + arma::exp(-eta), -1.0);
+//   }
+//   else if ( linkname == "inverse" ) {
+//     return arma::pow(eta, -1.0);
+//   }
+//   else if ( linkname == "probit" ) {
+//     for ( unsigned int i = 0; i < eta.size(); i++ ) {
+//       eta(i) = R::pnorm( eta(i), 0.0, 1.0, 1.0, 0.0 );    // R::pnorm( q, mean, sd, lower, log )
+//     }
+//     return eta;
+//   }
+//   else if ( linkname == "cauchit" ) {
+//     for ( unsigned int i = 0; i < eta.size(); i++ ) {
+//       eta(i) = R::pcauchy( eta(i), 0.0, 1.0, 1.0, 0.0 );    // R::pnorm( q, mean, sd, lower, log )
+//     }
+//     return eta;
+//   }
+//   else if ( linkname == "cloglog" ) {
+//     return ( 1 - arma::exp(-1.0 * arma::exp(eta) ) );
+//   }
+//   else if ( linkname == "sqrt" ) {
+//     return arma::square(eta);
+//   }
+//   else if ( linkname == "1/mu^2" ) {
+//     return arma::pow( arma::sqrt(eta), -1.0 );
+//   } else {
+//     stop("Invalid link function.");
+//   }
+//   return eta;
+// }
+// 
+// // [[Rcpp::export]]
+// double loglik_glm_cpp(
+//     const arma::vec& y,
+//     const arma::mat& X,
+//     const arma::vec& beta,
+//     const double& phi,
+//     const std::string& distname,
+//     const std::string& linkname
+// ) {
+//   int n         = y.size();
+//   double loglik = 0;
+//   arma::vec mu = invlink_cpp( X * beta, linkname );
+//   if ( distname == "gaussian" ) {
+//     for ( int i = 0; i < n; i++ ) {
+//       loglik += R::dnorm( y(i), mu(i), std::sqrt(phi), 1.0 );
+//     }
+//     return loglik;
+//   } 
+//   else if ( distname == "binomial" ) {
+//     for ( int i = 0; i < n; i++ ) {
+//       loglik += R::dbinom( y(i), 1, mu(i), 1.0 );
+//     }
+//     return loglik;
+//   } 
+//   else if ( distname == "poisson" ) {
+//     for ( int i = 0; i < n; i++ ) {
+//       loglik += R::dpois( y(i), mu(i), 1.0 );
+//     }
+//     return loglik;
+//   }
+//   else if ( distname == "Gamma" ) {
+//     double alpha = 1.0 / phi;
+//     mu = phi * mu;
+//     for ( int i = 0; i < n; i++ ) {
+//       loglik += R::dgamma( y(i), alpha, mu(i), 1.0 );
+//     }
+//     return loglik;
+//   } 
+//   else {
+//     return R_NegInf;
+//   }
+// }
 
 
